db2fc906651ac132ab995e1632e6510380497e6c
steven <langsford.steven@gmail.com>
2019-04-12 10:37:06 -0400

    working multitrial flipbook, savepoint before adding decision trajectory info-collection, might be a mess

---
+++ multitrial_flipbookplots/scratchpad.R
@@ -0,0 +1,18 @@
+#what I actually want:
+
+demo.df <- data.frame(trialID=c("demo1"),
+                      a.1=c(1),
+                      a.2=c(0),
+                      b.1=c(0),
+                      b.2=c(1),
+                      c.1=c(.9),
+                      c.2=c(.1),
+                      timestep=0,
+                      estvalA,
+                      estvalB,
+                      estvalC,
+                      esta1,
+                      esta2,
+                      esta3,
+                      currentbest,
+                      lastobs)
